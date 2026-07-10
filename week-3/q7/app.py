b71b58cb8a8178dc75a892cf1fc21a94f4eb43f7
k-k-0 <aadityyadav7531@gmail.com>
2026-07-10 11:28:13 +0530

    solve week 3 q7

--- week-3/q7/app.py
+++ week-3/q7/app.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjEwMDIwNTJAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MzY2MTg2NSwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NDI2NjY2NX0.bz34zqEgjZ3fR69QWSjN60c7l-DAZgyiGzcVzCaDjBo"
+# API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjEwMDIwNTJAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MzY2MTg2NSwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NDI2NjY2NX0.bz34zqEgjZ3fR69QWSjN60c7l-DAZgyiGzcVzCaDjBo"
 BASE_URL = os.environ.get("AIPIPE_BASE_URL", "https://aipipe.org/openai/v1")
 MODEL = os.environ.get("LLM_MODEL", "gpt-4o-mini")
 
